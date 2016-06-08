567446b4e43dddbad563a709890e4a3ee4e3a9b4
stain88 <marcbaghdadi@gmail.com>
2016-06-08 10:43:18 +0100

    setting up app.js

---
+++ app.js
@@ -0,0 +1,57 @@
+var express         = require('express'),
+    cors            = require('cors'),
+    path            = require('path'),
+    morgan          = require('morgan'),
+    bodyParser      = require('body-parser'),
+    mongoose        = require('mongoose'),
+    passport        = require('passport'),
+    cookieParser    = require('cookie-parser'),
+    methodOverride  = require('method-override'),
+    jwt             = require('jsonwebtoken'),
+    expressJWT      = require('express-jwt'),
+    app             = express();
+
+var config          = require('./config/config');
+var User            = require('./modles/user');
+var secret          = require('./config/config').secret;
+
+mongoose.connect(config.database);
+
+require('./config/passport')(passport);
+
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan('dev'));
+app.use(cors());
+app.use(passport.initialize());
+
+app.use('/api', expressJWT({ secret: secret })
+  .unless({
+    path: [
+      { url: '/api/login', methods: ['POST'] },
+      { url: '/api/register', methods: ['POST'] }
+    ]
+  }));
+
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    return res.status(401).json({message: 'Unauthorized request.'});
+  }
+  next();
+});
+
+var routes = require('./config/routes');
+app.use("/api", routes);
+
+app.listen(3000, function() {
+  console.log("Listening on port 3000");
+});
